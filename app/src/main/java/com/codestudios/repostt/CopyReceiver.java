ed99a3a34aa70b2c73414137501d13f568b345e7
Murtaz <murtazshah@gmail.com>
2017-01-11 15:10:26 +0500

    Boot Received

--- app/src/main/java/com/codestudios/repostt/CopyReceiver.java
+++ app/src/main/java/com/codestudios/repostt/CopyReceiver.java
@@ -4,8 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
 
 import static android.content.Context.CLIPBOARD_SERVICE;
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by Murtaz on 12/1/2016.
@@ -14,7 +17,10 @@ import static android.content.Context.CLIPBOARD_SERVICE;
 public class CopyReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        ClipboardManager clipBoard = (ClipboardManager)context.getSystemService(CLIPBOARD_SERVICE);
-        clipBoard.addPrimaryClipChangedListener( new ClipboardListener(context) );
+        Log.e("boot","received");
+        SharedPreferences preferences = context.getSharedPreferences("preferences",MODE_PRIVATE);
+        if(preferences.getBoolean("bService",false)){
+            context.startService(new Intent(context, ClipboardService.class));
+        }
     }
 }
